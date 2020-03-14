392e73c68fc76a01f0f728b40a0d4a25d90b5fb9
Chris Flick <chrisflick@gmail.com>
2020-03-14 14:44:28 -0700

    Updated readme

--- public/js/lobbies.js
+++ public/js/lobbies.js
@@ -1,4 +1,8 @@
 $(document).ready(() => {
+    searchLobbies()
+})
+
+function searchLobbies() {
     $.ajax({
         method: "GET",
         url: "/api/lobbies"
@@ -6,16 +10,16 @@ $(document).ready(() => {
         console.log(res)
 
         for (let i = 0; i < res.length; i++) {
-            let lobby = $("<div>")
-            let button = $("<button>")
+            let lobby = $("<div>");
+            let button = $("<button>");
 
-            lobby.html("<b> " + res[i].lobby_name + "</b>&nbsp; &nbsp; &nbsp;")
-            button.text('Join')
+            lobby.html("<b> " + res[i].lobby_name + "</b>&nbsp; &nbsp; &nbsp;");
+            button.text('Join');
 
-            button.attr("class", 'button is-link')
-            lobby.attr("class", "has-text-centered card")
-            button.attr("name", res[i].lobby_name)
-            button.attr("userID", res[i].id)
+            button.attr("class", 'button is-link');
+            lobby.attr("class", "has-text-centered card");
+            button.attr("name", res[i].lobby_name);
+            button.attr("userID", res[i].id);
 
 
             lobby.append(button)
@@ -23,9 +27,10 @@ $(document).ready(() => {
         }
 
         $(".button").on("click", function() {
-            localStorage.setItem("PeerChatID", $(this).attr("userID"))
-            localStorage.setItem("rm_name", $(this).attr("name"))
-            document.location.href = "/b"
+            localStorage.setItem("PeerChatID", $(this).attr("userID"));
+            localStorage.setItem("rm_name", $(this).attr("name"));
+            // document.location.href =
         })
     })
-})
+
+}
